54ac0a2e6663fc51667309287a107e8ae86fc96e
davidmj3434 <davidmj3434@gmail.com>
2024-05-17 19:21:02 +0000

    main

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 int	main(void)
 {
 	void	*mlx;
+	
 
+    
 	mlx = mlx_init();
     mlx_win = new_window(mlx,640,480, "hemlo world");
     mlx_loop(mlx);
-    
+    mlx_destroy_window(mlx, mlx_win);
+    mlx_terminate(mlx);
+    return 0;
 }
