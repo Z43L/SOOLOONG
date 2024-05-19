89fb775975f64e5335cdeb2e8474e29c76706caa
davidmj3434 <davidmj3434@gmail.com>
2024-05-19 23:19:07 +0000

    mas descuidos en el putnbr esta si q ssi

--- lib/FT_PRINTF/src/ft_putnbrd_base.c
+++ lib/FT_PRINTF/src/ft_putnbrd_base.c
@@ -6,7 +6,7 @@
 /*   By: davmoren <davmoren@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 19:10:37 by davmoren          #+#    #+#             */
-/*   Updated: 2024/05/19 23:16:51 by davmoren         ###   ########.fr       */
+/*   Updated: 2024/05/19 23:18:36 by davmoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int	printnbr(long c, int len, bool tof, int base)
 	hexmayus = "0123456789ABCDEF";
 	hexminus = "0123456789abcdef";
 	if (tof == true)
-		len += ft_putchard(hexmayus[c % base]);
+		len += ft_putchar_fd(hexmayus[c % base]);
 	else if (tof == false)
-		len += ft_putchard(hexminus[c % base]);
+		len += ft_putchar_fd(hexminus[c % base]);
 	else
-		len += ft_putchard(c % base + '0');
+		len += ft_putchar_fd(c % base + '0');
 	return (len);
 }
 
@@ -33,7 +33,7 @@ int	ft_putnbrd_base(long c, int base, int len, bool tof)
 {
 	if (c < 0)
 	{
-		len += ft_putchard('-');
+		len += ft_putchar_fd('-');
 		c = -c;
 	}
 	if (c >= base)
