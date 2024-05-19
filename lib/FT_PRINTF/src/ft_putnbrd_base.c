a959e5c68f048ddd7136c1ce87b22679465dabe7
davidmj3434 <davidmj3434@gmail.com>
2024-05-19 23:21:51 +0000

    todos estos me a pasado por perezoso

--- lib/FT_PRINTF/src/ft_putnbrd_base.c
+++ lib/FT_PRINTF/src/ft_putnbrd_base.c
@@ -6,12 +6,12 @@
 /*   By: davmoren <davmoren@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 19:10:37 by davmoren          #+#    #+#             */
-/*   Updated: 2024/05/19 23:18:36 by davmoren         ###   ########.fr       */
+/*   Updated: 2024/05/19 23:20:58 by davmoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../LIBFT/libft.h"
-#include <stdbool.h>
+#include "ft_printf.h"
 
 int	printnbr(long c, int len, bool tof, int base)
 {
@@ -21,11 +21,11 @@ int	printnbr(long c, int len, bool tof, int base)
 	hexmayus = "0123456789ABCDEF";
 	hexminus = "0123456789abcdef";
 	if (tof == true)
-		len += ft_putchar_fd(hexmayus[c % base]);
+		len += ft_putchard(hexmayus[c % base]);
 	else if (tof == false)
-		len += ft_putchar_fd(hexminus[c % base]);
+		len += ft_putchard(hexminus[c % base]);
 	else
-		len += ft_putchar_fd(c % base + '0');
+		len += ft_putchard(c % base + '0');
 	return (len);
 }
 
@@ -33,7 +33,7 @@ int	ft_putnbrd_base(long c, int base, int len, bool tof)
 {
 	if (c < 0)
 	{
-		len += ft_putchar_fd('-');
+		len += ft_putchard('-');
 		c = -c;
 	}
 	if (c >= base)
